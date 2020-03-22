7c7d98c06b7d591ae19cf0856186d483c97e801d
Yannick Rave <yannick@wrave.de>
2020-03-22 10:54:22 +0100

    Added Diary

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DiaryComponent } from 'src/app/modules/diary/diary/diary.component';
+import { QuestionnaireComponent } from 'src/app/modules/questionnaire/questionnaire/questionnaire.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: QuestionnaireComponent, pathMatch: 'full' },
+  { path: 'diary', component: DiaryComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/diary/diary.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DiaryComponent } from './diary/diary.component';
+
+
+
+@NgModule({
+  declarations: [DiaryComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class DiaryModule { }
